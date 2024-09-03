5e412c74bec5e5d3ec9376bc0f80baaadc25b62b
Lovethrech <rachealdolapo45@gmail.com>
2024-09-03 16:38:41 +0100

    starting with the mobile-display-only pricing component...

--- src/components/PFlash.vue
+++ src/components/PFlash.vue
@@ -1,3 +1,23 @@
+<style scoped>
+.ctn{
+    display:flex;
+    justify-content: center;
+    place-items:center;
+    width:100%;
+}
+.mini-ctn{
+    display:flex;
+    justify-content: center;
+    gap:10px;
+    font-size:14px;
+    background-color:rgba(0, 128, 0, 0.13);
+    color:green;
+    padding:10px 20px;
+    border-radius:20px;
+    border:2px solid green;
+}
+</style>
+
 <template>
     <div class="ctn">
         <div class="mini-ctn">
--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -4,14 +4,15 @@ import PFlash from "@/components/PFlash.vue";
 
 <style scoped>
 .p-ctn{
-    
+    padding:6vh 6vw;
 }
 </style>
 
 <template>
     <div class="p-ctn">
+        <PFlash></PFlash>
         <div class="mini-ctn">
-            <PFlash></PFlash>
+            
         </div>
     </div>
 </template>
