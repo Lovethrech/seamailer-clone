78563365be8489455cba6b444c5a5e82c62ff03f
Lovethrech <rachealdolapo45@gmail.com>
2024-09-03 18:46:31 +0100

    starting with the pricing card component..

--- src/components/PCardCtn.vue
+++ src/components/PCardCtn.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="main-ctn">
+        <div class="left-chevron-ctn"></div>
         <div class="mini-ctn">
             
         </div>
+        <div class="right-chevron-ctn"></div>
     </div>
 </template>
