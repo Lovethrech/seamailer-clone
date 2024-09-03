48c00642033696b9e829d4ef27163c9f55efb879
Lovethrech <rachealdolapo45@gmail.com>
2024-09-03 19:37:24 +0100

    working on the pricing card component...

--- src/components/PCard.vue
+++ src/components/PCard.vue
@@ -1,13 +1,56 @@
+<script setup>
+const {pricingInfo, cardWidth}=defineProps(['pricingInfo', 'cardWidth']);
+</script>
+
+<style scoped>
+.card-main-ctn{
+    background-color: white;
+    padding:20px;
+}
+.card-mini-ctn{
+    font-family: "Roboto Condensed", system-ui;
+}
+.sups,.subs{
+    color:#444444;
+    font-size:14px;
+}
+.name{
+    font-size:28px;
+    font-weight:700;
+}
+.list-ctn{
+    display:flex;
+    flex-direction: row;
+    gap:10px;
+    margin:10px 0;
+}
+.list-desc, .list-icon-ctn i{
+    display:flex;
+    place-items:center;
+    text-transform: capitalize;
+    font-size:16px;
+}
+</style>
+
 <template>
-    <div class="card-main-ctn">
+    <div class="card-main-ctn" :style="{width:cardWidth}">
         <div class="card-mini-ctn">
-            <p class="sups"></p>
-            <p class="name"></p>
-            <p class="subs"></p>
-            <div class="list-ctn">
+            <p class="sups">
+                {{ pricingInfo[0].sups }}
+            </p>
+            <p class="name">
+                {{ pricingInfo[0].name }}
+            </p>
+            <p class="subs">
+                {{ pricingInfo[0].subs }}
+            </p>
+            <div class="list-ctn" v-for="pricingList in pricingInfo[0].lists" :key="pricingList.id">
                 <div class="list-icon-ctn">
                     <i class='bx bxs-check-circle' ></i>
                 </div>
+                <p class="list-desc">
+                    {{ pricingList.desc}}
+                </p>
             </div>
         </div>
     </div>
--- src/components/PCardCtn.vue
+++ src/components/PCardCtn.vue
@@ -1,12 +1,21 @@
 <script setup>
+import {ref} from "vue";
 import PCard from "@/components/PCard.vue";
+import pricingInfos from "@/data/pricing-infos.json";
+
+const freeId=ref([pricingInfos[0]]);
+const freeCardWidth=ref("80%");
 </script>
 
 <template>
     <div class="main-ctn">
         <div class="left-chevron-ctn"></div>
         <div class="mini-ctn">
-            <PCrad></PCrad>
+            <PCard 
+                :pricingInfo="freeId"
+                :cardWidth="freeCardWidth"
+            >
+            </PCard>
         </div>
         <div class="right-chevron-ctn"></div>
     </div>
