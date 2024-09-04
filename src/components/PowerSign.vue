ae711b2d00455dbb06259c55fc268700cb575d9b
Lovethrech <rachealdolapo45@gmail.com>
2024-09-04 20:16:16 +0100

    starting with the power sign component...

--- src/components/PowerSign.vue
+++ src/components/PowerSign.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="main-ctn">
         <div class="mini-ctn">
-            
+            <h3 class="title">
+                Power your apps and users <br/> with transactional emails
+            </h3>
+            <p class="desc">
+                Boost your mobile 
+            </p>
         </div>
     </div>
 </template>
