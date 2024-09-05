e79107bc1eca7b76bbf334b7f7dd3dcc17970f97
Lovethrech <rachealdolapo45@gmail.com>
2024-09-05 21:01:18 +0100

    writing a function that toggles price components...

--- src/components/PCardCtn.vue
+++ src/components/PCardCtn.vue
@@ -4,13 +4,34 @@ import PCard from "@/components/PCard.vue";
 import pricingInfos from "@/data/pricing-infos.json";
 
 const freeId=ref([pricingInfos[0]]);
-const freeCardWidth=ref("80%");
+const freeCardWidth=ref("90vw");
+const displayLeftIcon=ref("none");
+
+const togglePrice=()=>{
+    
+}
 </script>
+
+<style scoped>
+.main-ctn{
+    display:flex;
+    flex-direction:row;
+    justify-content: space-between;
+    gap:10px;
+}
+.left-chevron-ctn, .right-chevron-ctn{
+    display:flex;
+    justify-content: center;
+    place-items:center;
+    font-size:40px;
+    width:30px;
+}
+</style>
 
 <template>
     <div class="main-ctn">
-        <div class="left-chevron-ctn">
-            <!-- <i class='bx bxs-chevron-left'></i> -->
+        <div class="left-chevron-ctn" >
+            <i class='bx bxs-chevron-left' :style="{display:displayLeftIcon}"></i>
         </div>
         <div class="mini-ctn">
             <PCard 
