37708c2955cb4217c5c08d79de4681847022a251
Lovethrech <rachealdolapo45@gmail.com>
2024-09-06 22:03:48 +0100

    working on the footer component...

--- src/components/Chel.vue
+++ src/components/Chel.vue
@@ -1,7 +1,15 @@
 <style scoped>
-
+p{
+    font-family: "Roboto Condensed", system-ui;
+    font-size:14px;
+}
+p a{
+    font-style:italic;
+    color:rgb(236, 217, 181);
+    font-weight:600;
+}
 </style>
 
 <template>
-    <p class="info">Modified by <a href="" target="_blank">Lovethrech</a></p>
+    <p class="info">Modified by <a href="https://github.com/Lovethrech/seamailer-clone" target="_blank">Lovethrech</a></p>
 </template>
