4ad57239f9dda14aeb8e884ebd8ead207df63e22
Lovethrech <rachealdolapo45@gmail.com>
2024-09-06 20:33:19 +0100

    added a blue-button component to the power-sign component...

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="main-ctn">
+        <div class="mini-ctn">
+            
+        </div>
+    </div>
+</template>
--- src/components/PowerSign.vue
+++ src/components/PowerSign.vue
@@ -1,3 +1,12 @@
+<script setup>
+import {ref} from "vue";
+import BlueBtn from "@/components/BlueBtn.vue";
+import navLists from "@/data/nav-list.json";
+
+const signUpId = ref([navLists[1]]);
+const btnWidth = ref("100%");
+</script>
+
 <style scoped>
 .main-ctn{
     background-color:#edf2fb;
@@ -11,6 +20,10 @@
 .title b{
     color: #f1914f;
 }
+.btn-ctn{
+    width:300px;
+    margin:0 auto;
+}
 </style>
 
 <template>
@@ -23,6 +36,16 @@
                 Boost your mobile &amp; web apps with our transactional emails. 
                 Get your notification updates emails to your users 100% of the time.
             </p>
+            <br/>
+            <br/>
+            <div class="btn-ctn">
+                <BlueBtn 
+                    :btnData="signUpId"
+                    :btnWidth="btnWidth"
+                >
+                </BlueBtn>
+            </div>
+            
         </div>
     </div>
 </template>
